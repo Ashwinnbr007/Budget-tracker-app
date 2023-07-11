ab6d3cd8ac21c2bd00bae151a3926d9d23f37a2c
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2023-07-11 21:28:30 +0530

    front end created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import './App.css';
 
+function CreateDate(){
+  const dateObject = new Date();
+  return dateObject.toISOString().split('T')[0]
+
+}
 function App() {
+  let todaysDate = CreateDate();
   return (
     <div className='main'>
       <h1>₹100<span>.00</span></h1>
       <form>
         <div className='Amount'>
-          <input placeholder = "Amount in number" type="number"></input>
+          <input placeholder = "₹" type="number"></input>
         </div>
+        <div className='AmountHint'>
+            <p className='left'> <mark className='red'>Negative</mark> for debited</p>
+            <p className='right'><mark className='green'>Positive</mark> for credited</p>
+          </div>
         <div className='Details'>
           <input placeholder = "Description of the item" type="desc"></input>
-          <input type="datetime-local"></input>
+          <input id='datePicker' defaultValue = {todaysDate} type="date"></input>
         </div>
         <button type='Submit'>
-          Add new Transaction
+          Add New Transaction
         </button>
       </form>
       <div className='Transactions'>
         <div className='Transaction'>
           <div className='Left'>
-            <div className='TransactionAmount'>₹500</div>
+            <div className='TransactionAmountRed'>₹500</div>
           </div>
           <div className='Right'>
-            <div className='TransactionDate'>2000-12-05</div>
             <div className='TransactionDetails'>Samsung tv</div>
+            <div className='TransactionDate'>05/12/2000</div>
+          </div>
+        </div>
+        <div className='Transaction'>
+          <div className='Left'>
+            <div className='TransactionAmountGreen'>₹500</div>
+          </div>
+          <div className='Right'>
+            <div className='TransactionDetails'>Samsung tv</div>
+            <div className='TransactionDate'>05/12/2000</div>
           </div>
         </div>
       </div>
